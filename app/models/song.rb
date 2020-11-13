20a69fc06b81fc90041ff4d4b028c763f3dc7c6d
adarvin14 <amdarvin14@gmail.com>
2020-11-13 15:10:54 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,23 +9,24 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name
-    self.artist.name = self.artist
+    self.artist ? self.artist.name : nil
   end
 
   def genre_id=(id)
-    self.genre = Genre.find_or_create_by(name:name)
+    self.genre = Genre.find_by(id: id)
   end
 
   def genre_id
     self.genre ? self.genre.name : nil
   end
 
-  def song_notes=(notes)
+  def note_contents=(notes)
     notes.each do |note|
       if note != ""
-        note = Note.find_or_create_by(content: content)
-        self.notes << note 
-      end 
+        note_instance = Note.find_or_create_by(content: note)
+        self.notes << note_instance 
+      end
     end 
   end
+  
 end
